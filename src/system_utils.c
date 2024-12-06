1540854f297c43270151e71e4752b78f9c54ac0f
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-12-06 09:56:14 +0300

    Update

--- src/system_utils.c
+++ src/system_utils.c
@@ -1,1 +1,21 @@
- 
+#include "system_utils.h"
+#include <stdio.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+    void clear_screen(void) {
+        HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD coord = {0, 0};
+        DWORD count;
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        GetConsoleScreenBufferInfo(hStdOut, &csbi);
+        FillConsoleOutputCharacter(hStdOut, ' ', 
+            csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+        SetConsoleCursorPosition(hStdOut, coord);
+    }
+#else
+    #include <unistd.h>
+    void clear_screen(void) {
+        printf("\033[2J\033[H");
+    }
+#endif 
